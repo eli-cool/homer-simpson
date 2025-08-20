3185e684c297ececfc6c224ac78cc0222412bccb
eli-cool <eh128257@student.wpsok.org>
2025-08-20 14:10:34 -0500

    Add files via upload

--- IM DEVING/scripts/dialouge_box.gd
+++ IM DEVING/scripts/dialouge_box.gd
@@ -2,15 +2,22 @@ extends Node2D
 
 
 @onready var plr = $"."
-
-	
-
+var can_speak := false
 
 func _ready() -> void:
-	$"Dog-haircut".visible = not $"Dog-haircut".visible
-
+	$"Dog-haircut".visible = false
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("+action"):
+		if can_speak:
+			$"Dog-haircut".visible = not $"Dog-haircut".visible
+	pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("RAAHHHHH")
-	$"Dog-haircut".visible = not $"Dog-haircut".visible
-	
+	can_speak = true
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	can_speak = false
+	$"Dog-haircut".visible = false
+	pass
