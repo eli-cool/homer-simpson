269d8e463ceec7390a51ad50c18d5812687a0dfe
eli-cool <eh128257@student.wpsok.org>
2025-08-22 14:09:23 -0500

    Add files via upload

--- IM DEVING/scripts/CharacterBody2D.gd
+++ IM DEVING/scripts/CharacterBody2D.gd
@@ -5,9 +5,8 @@ extends CharacterBody2D
 var walking:bool
 var mossy:String = "right"
 @export var still := false
-@onready var diolouge = $"."
-
-
+@onready var dialodge_box := $"player/dialouge box"
+@onready var text_label = $RichTextLabel
 func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,13 +34,6 @@ func _physics_process(_delta):
 	if not still:
 		move_and_slide()
 		anims(mossy)
-		
-	if diolouge.can_speak:
-		if Input.is_action_just_pressed("+action"):
-			$"Dog-haircut".visible = not $"Dog-haircut".visible
-		else:
-			$"Dog-haircut".visible = false
-
 
 func anims(moss):
 	"hi"
@@ -66,3 +58,10 @@ func anims(moss):
 			_animated_sprite.play("walk_east")
 		else:
 			_animated_sprite.play("idle_east")
+
+func _on_dialouge_box_dialodge(num_dia: int) -> void:
+	print("texting")
+	if num_dia == 1:
+		print("text2222")
+#		text_label.text = "hello im pie"
+		dialodge_box.modulate = Color("red")
--- IM DEVING/scripts/dialouge_box.gd
+++ IM DEVING/scripts/dialouge_box.gd
@@ -3,6 +3,17 @@ extends Node2D
 
 @onready var plr = $"."
 @export var can_speak := false
+signal dialodge(num_dia:int)
+signal hide
+
+func _process(delta: float) -> void:
+	if can_speak:
+		if Input.is_action_just_pressed("+action"):
+			$"Dog-haircut".visible = not $"Dog-haircut".visible
+			dialodge.emit(1)
+	else:
+		$"Dog-haircut".visible = false
+
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("RAAHHHHH")
