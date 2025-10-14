325e3bac7c3bb6599beb42b8c5b1bca70b22a4a6
eli-cool <eh128257@student.wpsok.org>
2025-10-14 13:56:17 -0500

    Add files via upload
    added delete, where you can shoot enemies

--- delete/bullet.gd
+++ delete/bullet.gd
@@ -4,22 +4,23 @@ const SPEED: int = 300
 @export var rotation_speed = 40
 
 func _ready():
-	add_to_group("bullets", true) # Add the bullet to a group for easier identification
+	add_to_group("bullets") # Add the bullet to a group for easier identification
 	
  
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
-	rotation_degrees += rotation_speed * delta
+#	rotation_degrees += rotation_speed * delta
 	
  
  
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
-	
-"""func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	#print(area)
-	if area.is_in_group("enemies"):
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print(area.get_parent())
+	if area.get_parent().is_in_group("enemies"):
 		print("hit")
 		queue_free()
+		area.get_parent().queue_free()
 	else:
-		print("nooooo")"""
+		print("nooooo") 
--- delete/enemy.gd
+++ delete/enemy.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
+class_name enemy
 
 @onready var player := $"../player"
 
 @export var speed := 50
 
+@onready var spr = $"Alien-cat"
+
 func _ready():
-	add_to_group("enemies", true) # Add the bullet to a group for easier identification
+	add_to_group("enemies") # Add the bullet to a group for easier identification
 	if is_in_group("enemies"):
 		print("whyyyy")
  
 func _physics_process(_delta):
+	rotation_degrees = wrap(rotation_degrees, 0, 360)
+	if rotation_degrees <270 and rotation_degrees>90:
+		spr.flip_v = true
+	else:
+		spr.flip_v = false
+	
+	#enemy move to player if player.
 	if not player:
 		return
 
